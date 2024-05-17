711f50a325a341a6aa748e23da7a0552f7749db8
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-17 15:58:15 +0200

    Update category.component.ts

--- src/app/layout/navbar/category/category.component.ts
+++ src/app/layout/navbar/category/category.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CategoryService } from './category.service';
+import { Category } from './category.model';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
 
+  categoryService:CategoryService = inject(CategoryService);
+
+  categories: Category[] | undefined;
+
+  currentActivateCategory: Category = this.categoryService.getCategoryByDefault();
+
+
+  ngOnInit(): void {
+      this.fetchCategories();
+  }
+  private fetchCategories() {
+    this.categories = this.categoryService.getCategories();
+  }
 }
